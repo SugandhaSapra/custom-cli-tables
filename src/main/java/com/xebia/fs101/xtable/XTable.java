499e6137cdc34a53529325e78b9ae1993015c99b
Sugandha Sapra <Sugandha.Sapra@xebia.com>
2019-10-15 12:01:03 +0530

    committed empty class file issue #6

--- src/main/java/com/xebia/fs101/xtable/XTable.java
+++ src/main/java/com/xebia/fs101/xtable/XTable.java
@@ -1,4 +1,17 @@
 package com.xebia.fs101.xtable;
 
 public class XTable {
+
+    private final String horizontalSep="-";
+    private final String verticalSep="|";
+
+    public void printRow()
+    {
+
+    }
+
+    public void printColumn()
+    {
+
+    }
 }

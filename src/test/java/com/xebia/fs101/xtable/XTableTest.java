7624bd5c32573844d72a36442d0bda17c4790a9c
Sugandha Sapra <Sugandha.Sapra@xebia.com>
2019-10-15 16:14:42 +0530

    Updated XTable.java file

---
+++ src/test/java/com/xebia/fs101/xtable/XTableTest.java
@@ -0,0 +1,14 @@
+package com.xebia.fs101.xtable;
+
+import org.junit.Test;
+
+public class XTableTest {
+
+    @Test
+    public void should_create_empty_table()
+    {
+      /*  XTable xTable=new XTable(1,1);
+        xTable.printTable();*/
+    }
+
+}

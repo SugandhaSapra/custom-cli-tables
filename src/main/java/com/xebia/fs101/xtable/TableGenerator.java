6c280988b9420c6c4ea6f144fabebc461e098b3d
Sugandha Sapra <Sugandha.Sapra@xebia.com>
2019-10-16 13:02:36 +0530

    Updated code for Empty Table Issue#6

---
+++ src/main/java/com/xebia/fs101/xtable/TableGenerator.java
@@ -0,0 +1,63 @@
+package com.xebia.fs101.xtable;
+
+public class TableGenerator implements TableGraphicsConst{
+
+    private int rows;
+    private int columns;
+    private int columnWidth;
+    TableGenerator(int rows, int columns)
+    {
+        this.rows=rows;
+        this.columns=columns;
+        columnWidth=10*columns;
+    }
+
+    public void printTable() {
+        printTopLine();
+        printMidLines();
+        printBottomLine();
+    }
+
+    public void printTopLine() {
+        int columnWidth = 10 * columns;
+        for (int i = 1; i <= columnWidth; i++) {
+            if (i == 1)
+                System.out.print(topLeft);
+            if (i == columnWidth)
+                System.out.print(topRight);
+            else if (i % 10 == 0)
+                System.out.print(topMiddle);
+            else
+                System.out.print(mid);
+        }
+    }
+
+    public void printMidLines() {
+
+        for (int j = 1; j < rows; j++) {
+            System.out.print("\n" + leftMid);
+            for (int i = 1; i < columnWidth; i++) {
+                if (i % 10 == 0)
+                    System.out.print(midMid);
+                else
+                    System.out.print(mid);
+            }
+            System.out.print(rightMid);
+        }
+        System.out.println();
+    }
+
+    public void printBottomLine() {
+
+        for (int i = 1; i <= columnWidth; i++) {
+            if (i == 1)
+                System.out.print(bottomLeft);
+            if (i == columnWidth)
+                System.out.print(bottomRight);
+            else if (i % 10 == 0)
+                System.out.print(bottomMiddle);
+            else
+                System.out.print(mid);
+        }
+    }
+}

263c242ded59305dc393ce69aec43c12a17160f4
Sugandha Sapra <Sugandha.Sapra@xebia.com>
2019-10-17 22:06:53 +0530

    Updated code as per today suggestions Issue #6

--- src/main/java/com/xebia/fs101/xtable/Table.java
+++ src/main/java/com/xebia/fs101/xtable/Table.java
@@ -3,9 +3,13 @@ package com.xebia.fs101.xtable;
 public class Table {
 
     private TableLayoutManager tableLayoutManager;
+    private int rowCount;
+    private int colCount;
 
     public Table(int rowCount, int colCount) {
         tableLayoutManager = new TableLayoutManager(rowCount, colCount);
+        this.rowCount=rowCount;
+        this.colCount=colCount;
 
     }
 

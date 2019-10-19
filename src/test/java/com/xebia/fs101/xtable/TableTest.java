101865e2e158a7cfb9ef761fff2ed0d342832c86
Sugandha Sapra <Sugandha.Sapra@xebia.com>
2019-10-19 12:41:02 +0530

    Updated code as per today suggestions Issue #6

--- src/test/java/com/xebia/fs101/xtable/TableTest.java
+++ src/test/java/com/xebia/fs101/xtable/TableTest.java
@@ -9,9 +9,9 @@ public class TableTest {
     @Test
     public void should_create_table_with_no_data_with_for_1_row_and_col() {
         Table table = new Table(1, 1);
-        String actualResult = table.generateTable();
+        String actualResult = table.createTable();
         String expectedResult =
-                "┌───────────────────┐\n" +
+                        "┌───────────────────┐\n" +
                         "│                   │\n" +
                         "└───────────────────┘";
         assertThat(actualResult).isEqualTo(expectedResult);
@@ -20,9 +20,9 @@ public class TableTest {
     @Test
     public void should_create_table_with_no_data_for_2_row_and_cols() {
         Table table = new Table(2, 2);
-        String actualResult = table.generateTable();
+        String actualResult = table.createTable();
         String expectedResult =
-                "┌───────────────────┬───────────────────┐\n" +
+                        "┌───────────────────┬───────────────────┐\n" +
                         "│                   │                   │\n" +
                         "├───────────────────┼───────────────────┤\n" +
                         "│                   │                   │\n" +
@@ -33,9 +33,9 @@ public class TableTest {
     @Test
     public void should_create_table_with_no_data_for_2_row_and_3cols() {
         Table table = new Table(2, 3);
-        String actualResult = table.generateTable();
+        String actualResult = table.createTable();
         String expectedResult =
-                "┌───────────────────┬───────────────────┬───────────────────┐\n" +
+                        "┌───────────────────┬───────────────────┬───────────────────┐\n" +
                         "│                   │                   │                   │\n" +
                         "├───────────────────┼───────────────────┼───────────────────┤\n" +
                         "│                   │                   │                   │\n" +
@@ -47,7 +47,7 @@ public class TableTest {
     @Test
     public void should_return_shape_of_table_when_given_valid_data() {
         Table table = new Table(3, 4);
-        String actualResult = table.shape();
+        String actualResult = table.getShape();
         String expectedResult = "3X4";
         assertThat(actualResult).isEqualTo(expectedResult);
     }

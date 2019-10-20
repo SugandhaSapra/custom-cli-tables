28645db9efd3a5e15bb56bd7f494806da4ff775e
Sugandha Sapra <Sugandha.Sapra@xebia.com>
2019-10-20 09:49:20 +0530

    Added logic for Data Tables

--- src/main/java/com/xebia/fs101/xtable/Table.java
+++ src/main/java/com/xebia/fs101/xtable/Table.java
@@ -1,30 +1,38 @@
 package com.xebia.fs101.xtable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Table {
 
     private int rowCount;
     private int colCount;
     private TableLayoutManager tableLayoutManager;
     private Renderer renderer;
+    private List<String[]> rows=new ArrayList<>();;
 
 
     Table(int rowCount, int colCount) {
+
         tableLayoutManager = new TableLayoutManager(rowCount, colCount);
         if (rowCount <= 0 || colCount <= 0)
             throw new IllegalArgumentException("Row and Col should be greater than 0");
         this.rowCount = rowCount;
         this.colCount = colCount;
         renderer = new ConsoleBaseRenderer();
+
+
     }
 
-    Table(int rowCount, int colCount, Renderer renderer) {
+   /* Table(int rowCount, int colCount, Renderer renderer) {
         tableLayoutManager = new TableLayoutManager(rowCount, colCount);
         if (rowCount <= 0 || colCount <= 0)
             throw new IllegalArgumentException("Row and Col should be greater than 0");
         this.rowCount = rowCount;
         this.colCount = colCount;
         renderer = this.renderer;
-    }
+
+    }*/
 
     public String getShape() {
         return rowCount + " rows X " + colCount + " cols";
@@ -32,7 +40,7 @@ public class Table {
 
     public String generateTable() {
 
-        return tableLayoutManager.createTable();
+        return tableLayoutManager.createTable(rows);
 
     }
 
@@ -40,5 +48,9 @@ public class Table {
         renderer.printTable(generateTable());
     }
 
+    public void addRow(String... cells) {
+       rows.add(cells);
 
-}
+    }
+
+  }
--- src/test/java/com/xebia/fs101/xtable/TableLayoutManagerTest.java
+++ src/test/java/com/xebia/fs101/xtable/TableLayoutManagerTest.java
@@ -2,6 +2,9 @@ package com.xebia.fs101.xtable;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
@@ -10,7 +13,10 @@ public class TableLayoutManagerTest {
     @Test
     public void should_create_table_with_no_data_for_2_row_and_cols() {
         TableLayoutManager tableLayoutManager = new TableLayoutManager(1, 2);
-        String actualResult = tableLayoutManager.createTable();
+        String[] cells={"one","two"};
+        List<String[]> rows=new ArrayList<>();
+        rows.add(cells);
+        String actualResult = tableLayoutManager.createTable(rows);
         String expectedResult =
                                 "┌───────────────────┬───────────────────┐\n" +
                                 "│                   │                   │\n" +
